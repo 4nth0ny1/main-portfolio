d2f4f1de52018cd53ccf3d1583bfbb3c17a6aaf7
4nth0ny1 <acatullo4@gmail.com>
2023-09-11 18:22:23 -0500

    updated home page and colors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,24 @@
 export default function Navbar() {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-white">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl normal-case">daisyUI</a>
+        <a className="btn btn-ghost text-xl normal-case text-black">
+          Anthony's Portfolio
+        </a>
       </div>
       <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
+        <ul className="menu menu-horizontal px-1 text-black">
           <li>
-            <a>Link</a>
+            <a>Home</a>
           </li>
           <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="bg-base-100 p-2">
-                <li>
-                  <a>Link 1</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-              </ul>
-            </details>
+            <a>About</a>
+          </li>
+          <li>
+            <a>Projects</a>
+          </li>
+          <li>
+            <a>Contact</a>
           </li>
         </ul>
       </div>
