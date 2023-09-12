4f38dddeb65139a1acd21c87b8db68b8c63a1da1
4nth0ny1 <acatullo4@gmail.com>
2023-09-11 22:40:42 -0500

    navbar changes to darkmode when about is clicked and vice versa now

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type NavBarProps = {
   handleProjectClick: () => void;
   handleAboutClick: () => void;
@@ -11,25 +13,33 @@ export default function Navbar({
   handleHomeClick,
   handleContactClick,
 }: NavBarProps) {
+  const [bgColor, setBgColor] = useState("bg-white");
+
+  const changeBgColor = () => {
+    setBgColor("bg-black text-white");
+  };
+
+  const revertBgColor = () => {
+    setBgColor("bg-white text-black");
+  };
+
   return (
-    <div className="navbar sticky top-0 z-50 bg-white">
+    <div className={`navbar sticky top-0 z-50 ${bgColor}`}>
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl normal-case text-black">
-          Anthonys Portfolio
-        </a>
+        <a className="btn btn-ghost text-xl normal-case ">Anthonys Portfolio</a>
       </div>
       <div className="flex-none">
-        <ul className="menu menu-horizontal px-1 text-black">
-          <li>
+        <ul className="menu menu-horizontal px-1 ">
+          <li onClick={revertBgColor}>
             <a onClick={handleHomeClick}>Home</a>
           </li>
-          <li>
+          <li onClick={changeBgColor}>
             <a onClick={handleAboutClick}>About</a>
           </li>
-          <li>
+          <li onClick={revertBgColor}>
             <a onClick={handleProjectClick}>Projects</a>
           </li>
-          <li>
+          <li onClick={revertBgColor}>
             <a onClick={handleContactClick}>Contact</a>
           </li>
         </ul>
