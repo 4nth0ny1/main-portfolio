c975d0a78321c96b58948bb7ec539e921fee7565
4nth0ny1 <acatullo4@gmail.com>
2023-09-11 22:02:54 -0500

    added sticky navbar and completed scroll to section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,18 @@
 type NavBarProps = {
   handleProjectClick: () => void;
   handleAboutClick: () => void;
+  handleHomeClick: () => void;
+  handleContactClick: () => void;
 };
 
 export default function Navbar({
   handleProjectClick,
   handleAboutClick,
+  handleHomeClick,
+  handleContactClick,
 }: NavBarProps) {
   return (
-    <div className="navbar bg-white">
+    <div className="navbar sticky top-0 z-50 bg-white">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl normal-case text-black">
           Anthonys Portfolio
@@ -17,7 +21,7 @@ export default function Navbar({
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1 text-black">
           <li>
-            <a>Home</a>
+            <a onClick={handleHomeClick}>Home</a>
           </li>
           <li>
             <a onClick={handleAboutClick}>About</a>
@@ -26,7 +30,7 @@ export default function Navbar({
             <a onClick={handleProjectClick}>Projects</a>
           </li>
           <li>
-            <a>Contact</a>
+            <a onClick={handleContactClick}>Contact</a>
           </li>
         </ul>
       </div>
