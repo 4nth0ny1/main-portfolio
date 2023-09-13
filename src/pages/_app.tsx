22eed5124aa14844e61be9b5c33b400d484193f5
4nth0ny1 <acatullo4@gmail.com>
2023-09-12 23:10:09 -0500

    added first animation to loading screen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 import "~/styles/globals.css";
 
@@ -7,10 +8,27 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => setLoading(!loading), 2000);
+    setTimeout(() => setLoading(!loading), 9000);
   }, []);
 
-  return <>{!loading ? "Loading" : <Component {...pageProps} />}</>;
+  return (
+    <>
+      {!loading ? (
+        <main className="flex min-h-screen flex-col items-center justify-center bg-white">
+          <motion.h1
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 4, duration: 2, ease: [0, 0.71, 0.2, 1.01] }}
+            className="pb-4 text-5xl"
+          >
+            My name is Anthony, and this my portfolio.
+          </motion.h1>
+        </main>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </>
+  );
 };
 
 export default MyApp;
