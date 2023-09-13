06bf666953393ae9fd962b73de0404dd2e1204b9
4nth0ny1 <acatullo4@gmail.com>
2023-09-12 22:48:49 -0500

    created loading screen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import React, { useEffect, useState } from "react";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  );
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(!loading), 2000);
+  }, []);
+
+  return <>{!loading ? "Loading" : <Component {...pageProps} />}</>;
 };
 
 export default MyApp;
