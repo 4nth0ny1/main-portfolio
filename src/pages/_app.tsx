d731d08437775ec0459e6636275b4c27c966534e
4nth0ny1 <acatullo4@gmail.com>
2023-09-14 23:28:44 -0500

    added svg and using framer motion to animate preloader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,26 +3,28 @@ import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 import "~/styles/globals.css";
+import Preloader from "~/components/Preloader";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => setLoading(!loading), 9000);
+    setTimeout(() => setLoading(!loading), 3000);
   }, []);
 
   return (
     <>
       {!loading ? (
-        <main className="flex min-h-screen flex-col items-center justify-center bg-white">
-          <motion.h1
+        <main className="flex min-h-screen flex-col items-center justify-center bg-[#141414]">
+          {/* <motion.h1
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ delay: 4, duration: 2, ease: [0, 0.71, 0.2, 1.01] }}
             className="pb-4 text-5xl"
           >
             My name is Anthony, and this my portfolio.
-          </motion.h1>
+          </motion.h1> */}
+          <Preloader />
         </main>
       ) : (
         <Component {...pageProps} />
